e4810a6fe90d987b8cc320bd0f24ed1276dedf94
kanned1079 <kanned1079@gmail.com>
2025-05-17 16:34:41 +0900

    update May 17

--- note_forest_server/src/app.module.ts
+++ note_forest_server/src/app.module.ts
@@ -7,6 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
 import { KnowledgeModule } from './knowledge/knowledge.module';
 import { CommentModule } from './comment/comment.module';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
     imports: [
@@ -14,15 +15,32 @@ import { CommentModule } from './comment/comment.module';
             secret: 'your-secret-key', // 替换为你的实际密钥
             signOptions: { expiresIn: '1h' }, // 设置令牌过期时间
         }),
-        TypeOrmModule.forRoot({
-            type: 'mysql',           // 或 'postgres' / 'sqlite' / 'mariadb'
-            host: '192.168.0.243',
-            port: 3306,
-            username: 'note',
-            password: 'Passcode1!',
-            database: 'note_forest',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true,       // 开发环境设为 true，自动建表
+        // TypeOrmModule.forRoot({
+        //     type: 'mysql',           // 或 'postgres' / 'sqlite' / 'mariadb'
+        //     host: '192.168.0.243',
+        //     port: 3306,
+        //     username: 'note',
+        //     password: 'Passcode1!',
+        //     database: 'note_forest',
+        //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        //     synchronize: true,       // 开发环境设为 true，自动建表
+        // }),
+        // TypeORM 异步配置，读取环境变量
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'mysql',
+                host: config.get('DB_HOST'),
+                port: parseInt(config.get('DB_PORT', '3306')),
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_DATABASE'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
         }),
         UserModule,
         AuthModule,
